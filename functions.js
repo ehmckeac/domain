40f2a0ea56454e925f08f94278a91f84e6fe92e5
ehmckeac <59941749+ehmckeac@users.noreply.github.com>
2020-04-27 00:53:16 -0400

    arrow nav

--- functions.js
+++ functions.js
@@ -1,9 +1,18 @@
-// 
+//
 // $( function() {
 //   $( ".resizable" ).resizable();
 // } );
 
 
-$( function() {
-  $( ".draggable" ).draggable();
+$(function() {
+  $(".draggable").draggable();
 } );
+
+
+$(document).ready(function() {
+    $("h2").click(function() {
+        $("html, body").animate({
+            scrollTop: $('html, body').get(0).scrollHeight
+        }, 2000);
+    });
+});
